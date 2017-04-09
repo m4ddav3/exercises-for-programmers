9dc314f92db0085b1cebf6187fcca9902dd4e6ce
m4ddav3 <m4ddav3@users.noreply.github.com>
2017-04-09 22:07:35 +0000

    Add prompt_and_greet function

--- elixir/ex_01_say_hello/lib/ex_01_say_hello.ex
+++ elixir/ex_01_say_hello/lib/ex_01_say_hello.ex
@@ -15,4 +15,16 @@ defmodule Ex01SayHello do
   def hello do
     :world
   end
+
+
+  @doc """
+  Prompt for the user's name, then print a greeting
+  using that name
+
+  """
+  def prompt_and_greet do
+    name = IO.gets "What is your name? "
+    name = String.trim_trailing(name, "\n")
+    IO.puts "Hello #{name}, nice to meet you!"
+  end
 end
